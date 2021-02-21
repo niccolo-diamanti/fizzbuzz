671540534c156d56e3c679b913dfa7b4210b199d
Niccolò Diamanti <niccolo.diamanti@gmail.com>
2021-02-21 18:37:02 +0100

    game handle custom substitutions

--- src/main/java/com/ndiamanti/fizzbuzz/FizzBuzzGame.java
+++ src/main/java/com/ndiamanti/fizzbuzz/FizzBuzzGame.java
@@ -1,14 +1,29 @@
 package com.ndiamanti.fizzbuzz;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class FizzBuzzGame {
 
+    Function<Integer, String> fizz = i -> i % 3 == 0 ? "fizz" : "";
+    Function<Integer, String> buzz = i -> i % 5 == 0 ? "buzz" : "";
+    List<Function<Integer, String>> functionList = Arrays.asList(fizz, buzz);
+
     public String play(Integer number) {
-        if (number % 15 == 0)
-            return "fizz buzz";
-        else if (number % 3 == 0)
-            return "fizz";
-        else if (number % 5 == 0)
-            return "buzz";
-        return String.valueOf(number);
+        return play(number, null);
+    }
+
+    public String play(Integer number, List<Function<Integer, String>> substitutionFunctionList) {
+        String result = evaluateSubstitutionFunctionList(number, substitutionFunctionList != null ? substitutionFunctionList : functionList);
+        return result.isEmpty() || result.isBlank() ? String.valueOf(number) : result;
+    }
+
+    private String evaluateSubstitutionFunctionList(Integer number, List<Function<Integer, String>> substitutionFunctionList) {
+        return substitutionFunctionList.stream()
+                .map(function -> function.apply(number))
+                .collect(Collectors.joining(" "))
+                .trim();
     }
 }
--- src/main/java/com/ndiamanti/fizzbuzz/FizzBuzzPop.java
+++ src/main/java/com/ndiamanti/fizzbuzz/FizzBuzzPop.java
@@ -1,23 +1,18 @@
 package com.ndiamanti.fizzbuzz;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
 public class FizzBuzzPop extends FizzBuzzGame {
+
+    Function<Integer, String> fizz = i -> i % 3 == 0 ? "fizz" : "";
+    Function<Integer, String> buzz = i -> i % 5 == 0 ? "buzz" : "";
+    Function<Integer, String> pop = i -> i % 7 == 0 ? "pop" : "";
+    List<Function<Integer, String>> functionList = Arrays.asList(fizz, buzz, pop);
 
     @Override
     public String play(Integer number) {
-        if (number % 105 == 0) {
-            return "fizz buzz pop";
-        } else if (number % 35 == 0) {
-            return "buzz pop";
-        } else if (number % 21 == 0) {
-            return "fizz pop";
-        } else if (number % 7 == 0) {
-            return "pop";
-        } else if (number % 15 == 0)
-            return "fizz buzz";
-        else if (number % 3 == 0)
-            return "fizz";
-        else if (number % 5 == 0)
-            return "buzz";
-        return String.valueOf(number);
+        return super.play(number, functionList);
     }
 }

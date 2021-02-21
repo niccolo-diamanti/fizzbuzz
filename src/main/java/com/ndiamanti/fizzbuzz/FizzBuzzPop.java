583e1cb373588465bea985060cf66828c43f0b5c
Niccolò Diamanti <niccolo.diamanti@gmail.com>
2021-02-21 15:32:53 +0100

    buzz pop for multiples of five and seven

--- src/main/java/com/ndiamanti/fizzbuzz/FizzBuzzPop.java
+++ src/main/java/com/ndiamanti/fizzbuzz/FizzBuzzPop.java
@@ -4,7 +4,9 @@ public class FizzBuzzPop extends FizzBuzzGame {
 
     @Override
     public String play(Integer number) {
-        if (number % 21 == 0) {
+        if (number % 35 == 0) {
+            return "buzz pop";
+        } else if (number % 21 == 0) {
             return "fizz pop";
         } else if (number % 7 == 0) {
             return "pop";
